8e88271a3e86502ff8c6ee71bb1f6e4b829c2a87
gsilis <girts.silis@omf.com>
2021-02-25 13:44:59 -0600

    + message list and message component shell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MessageListComponent } from './message-list/message-list.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageListComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,

1e3ebd4756c70ecf4be133783f42d25bf900d54a
gsilis <girts.silis@omf.com>
2021-02-25 14:14:36 -0600

    + dynamic form title
    + output for message form

--- src/app/message-form/message-form.component.ts
+++ src/app/message-form/message-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Message } from '../message';
 
 @Component({
   selector: 'app-message-form',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageFormComponent implements OnInit {
 
+  title: string = '';
+  message: string = '';
+
+  @Input() formTitle: string;
+  @Output() onComplete: EventEmitter<Message> = new EventEmitter<Message>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  get hasMessage():boolean {
+    return this.title.length > 0 && this.message.length > 0;
+  }
+
+  onSubmit(): void {
+    this.onComplete.emit({ title: this.title, body: this.message });
+    this.title = this.message = '';
   }
 
 }

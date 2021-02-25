1aa07f7c675216baefcecfe8b32699104058caea
gsilis <girts.silis@omf.com>
2021-02-25 13:46:54 -0600

    + shell message form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MessageListComponent } from './message-list/message-list.component';
 import { MessageComponent } from './message/message.component';
+import { MessageFormComponent } from './message-form/message-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MessageListComponent,
-    MessageComponent
+    MessageComponent,
+    MessageFormComponent
   ],
   imports: [
     BrowserModule,
